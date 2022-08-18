14d2a21ccbe4c2186c25b3d35b4331a9004219cd
Alexander Emelyanov <35880220+Homosanians@users.noreply.github.com>
2022-08-18 23:16:21 +0300

    Scan every directory

--- core/config.py
+++ core/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 IMAGES_PATH = os.environ.get("IMAGES_PATH", './memes')
+PENDING_PATH = os.environ.get("PENDING_PATH", './memes_pending')
 TRAIN_PATH = os.environ.get("TRAIN_PATH", './dataset')
 
 LOGGING_LEVEL = int(os.environ.get('LOGGING_LEVEL', 1))  # 0 - DEBUG, 1 - INFO, 2 - WARN, 3 - ERR, 4 - CRITICAL (FATAL)
@@ -14,3 +15,5 @@ SCRAP_PERIOD_MINUTES = int(os.environ.get('SCRAP_PERIOD_MINUTES', 90))
 MEME_SCAN_PERIOD_SECONDS = int(os.environ.get('MEME_SCAN_PERIOD_SECONDS', 30))
 ROTATION_POST_LIFESPAN_HOURS = int(os.environ.get('ROTATION_POST_LIFESPAN_HOURS', 72))
 ROTATION_KEEP_FILES_COUNT = int(os.environ.get('ROTATION_KEEP_FILES_COUNT', 1000))
+
+DUPLICATE_DETECT_THRESHOLD = float(int(os.environ.get('DUPLICATE_DETECT_THRESHOLD', 0.1)))  # 0 - absolute copy
--- scrapping/base_scrapper.py
+++ scrapping/base_scrapper.py
@@ -13,8 +13,11 @@ class BaseScrapper:
     def get_images(self):
         pass
 
+    # TODO Scanning each time is not efficient
     def get_unique_name(self):
-        files = list(map(lambda x: os.path.basename(x), glob.glob(os.path.join(config.IMAGES_PATH, "*"))))
+        files = list(map(lambda x: os.path.basename(x), glob.glob(os.path.join(config.IMAGES_PATH, "*")))) + \
+                list(map(lambda x: os.path.basename(x), glob.glob(os.path.join(config.PENDING_PATH, "*")))) + \
+                list(map(lambda x: os.path.basename(x), glob.glob(os.path.join(config.TRAIN_PATH, "*"))))
         while True:
             unique_name = str(uuid.uuid4())
             if unique_name not in files:
